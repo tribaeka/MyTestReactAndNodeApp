59fe1b34580c49892bd5436edd24f4765ced0bbd
tribaeka <egor924462@gmail.com>
2019-12-13 17:53:48 +0300

    home link on navbar correcting

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -12,10 +12,7 @@ export default function AppHeader({loadOptions}) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">SomeFutureItem</a>
+                            <a className="nav-link" href="/">Home</a>
                         </li>
                     </ul>
                     <FilterJobsForm loadOptions={loadOptions}/>
