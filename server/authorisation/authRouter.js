f00f961dcd01c97db3997f431b8e93a69b615a8c
tribaeka <egor924462@gmail.com>
2019-12-19 10:05:10 +0300

    to merge without complete server auth logic

--- server/authorisation/authRouter.js
+++
@@ -1,8 +0,0 @@
-module.exports = (router, expressApp, authRoutesMethods) => {
-
-    router.post('/registerUser', authRoutesMethods.registerUser);
-
-    router.post('/login’, expressApp.oauth.grant()');
-
-    return router
-};
